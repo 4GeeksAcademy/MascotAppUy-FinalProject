e67c4798c710fb5bcfe1e22fe31df044d2d5b39a
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-08-06 16:00:16 +0000

    classes added and 1 relationship

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Mascota, Especie, Raza, Departamento, Localidad, Color
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,12 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Mascota, db.session))
+    admin.add_view(ModelView(Especie, db.session))
+    admin.add_view(ModelView(Raza, db.session))
+    admin.add_view(ModelView(Departamento, db.session))
+    admin.add_view(ModelView(Localidad, db.session))
+    admin.add_view(ModelView(Color, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+from enum import Enum, auto
 
 db = SQLAlchemy()
 
@@ -6,7 +8,11 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    nombre = db.Column(db.String(50), nullable=False)
+    fecha_registro = db.Column(db.Date, default=date.today())
+    telefono = db.Column(db.String, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    mascotas = db.relationship('Mascota', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +21,103 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "nombre": self.nombre,
+            "fecha_registro": self.fecha_registro,
+            "telefono": self.telefono,
+            "is_active": self.is_active
         }
+
+class Estado(Enum):
+    perdido = auto()
+    encontrado = auto()
+    adopcion = auto()
+    reunido = auto()
+
+class Mascota(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), nullable=False)
+    edad = db.Column(db.String(120), nullable=False)
+    descripcion = db.Column(db.String(250), nullable=False)
+    estado = db.Column(db.Enum(Estado), nullable=False)
+    fecha_registro = db.Column(db.Date, default=date.today())
+    fecha_perdido = db.Column(db.Date, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<User {self.nombre}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "fecha_registro": self.fecha_registro,
+            "edad": self.edad,
+            "estado": self.estado,
+            "descripcion": self.descripcion
+        }
+
+class Especie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }   
+
+class Raza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+class Departamento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+    
+class Localidad(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+    
+class Color(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return '<Characters %r>' % self.name
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        } 
