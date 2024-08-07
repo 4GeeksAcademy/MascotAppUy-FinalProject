1bbe77ce9f82dca216b083dcdf7992dff9199a3e
Letimachado <leti.machado.gerez@gmail.com>
2024-08-07 15:22:44 +0000

    creando navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoClaro from "../../img/logoClaro.png"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-dark" style={{backgroundColor: "#040926", color: "#E0E1DD"}}>
+			<div className="conteiner-fluid">
+				<div className="row">
+					<div className="col-6">
+						<img src={logoClaro} alt="Logo" style={{width: "80px", height: "80"}}/>
+						<h3>MascotApp</h3>
+					</div>
+					<div className="col-6">
+						<p>Encontradas</p>
+					</div>
 				</div>
 			</div>
 		</nav>
+
+
+
+
 	);
 };
