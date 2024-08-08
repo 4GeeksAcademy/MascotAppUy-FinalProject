7e4f353002b4358f3f17b10e7582d559ba0031b0
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-08-08 15:59:22 +0000

    agregando algunos filtros

--- src/front/js/component/filtros.jsx
+++ src/front/js/component/filtros.jsx
@@ -4,19 +4,25 @@ import { Context } from "../store/appContext.js";
 
 const Filtros = () => {
     const { store } = useContext(Context)
+    //agregar logica del filtro
 
     console.log(store.mascotas)
     return(
         <div className="container">
-            <div className="filtro-especie">
-                <p className="filtro-label">Especie </p>
+
+            <div className="row mt-4">
+                <div className="col-3 d-flex">
+                    <p className="filtro-label me-3">Especie </p>
+
+                    <select className="form-select" aria-label="Default select example">
+                            
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+
+                </div>
                 
-                <select class="form-select" aria-label="Default select example">
-                    
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                </select>
             </div>
             
         </div>
