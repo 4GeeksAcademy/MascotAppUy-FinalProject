fba1a13c081278c6640975132b23ea0834e547c3
Trilce Godoy <112527761+tgodoy12@users.noreply.github.com>
2024-08-20 20:15:26 +0000

    styling landing page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -17,12 +17,14 @@ export const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid"> 
             <div className="collapse navbar-collapse d-flex" id="navbarSupportedContent">
-                <div className="icon image-container">
-                    <Link to="/">
+                <div className="image-container d-flex align-items-center pe-0 me-0">
+                   <Link to="/">
                         <img src={logoClaro} alt="Static Image" className="static-image pb-0" />
-                        <img src={logoClaroGif} alt="Animated Image" className="animated-image pb-0" />
+                        <img src={logoClaroGif} alt="Animated Image" className="animated-image pb-0" />    
                     </Link>
+                      
                 </div>
+                <span>MascotApp</span>  
             <ul className="navbar-nav ms-auto">
                 <li className="nav-item d-flex justify-content-end">
                     <Link to="/mascotas-encontradas" className="nav-link"><button type="button" className="button-no-border btn btn-outline-light">Encontradas</button></Link>
@@ -65,58 +67,6 @@ export const Navbar = () => {
         </div>
     </nav>
   )
-
-//       <nav className="navbar navbar-expand-md navbar-dark sticky-top" style={{ backgroundColor: "#040926", color: "#E0E1DD" }}>
-//           <div className="container">
-//               <button id="botonPrincipal" className="image-container pb-0" onClick={() => nav("/")}>
-//                   <img src={logoClaro} alt="Static Image" className="static-image pb-0" />
-//                   <img src={logoClaroGif} alt="Animated Image" className="animated-image pb-0" />
-//               </button>
-//               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//                   <span className="navbar-toggler-icon"></span>
-//               </button>
-//               <div className="collapse navbar-collapse" id="navbarNav">
-//                   <ul className="navbar-nav ms-auto">
-//                       <li className="nav-item d-flex justify-content-end">
-//                           <Link to="/mascotas-encontradas" className="nav-link">Encontradas</Link>
-//                       </li>
-//                       <li className="nav-item d-flex justify-content-end">
-//                           <Link to="/mascotas-perdidas" className="nav-link">Perdidas</Link>
-//                       </li>
-//                       <li className="nav-item d-flex justify-content-end">
-//                           <Link to="/mapa" className="nav-link">Mapa</Link>
-//                       </li>
-//                       <li className="nav-item d-flex justify-content-end">
-//                           <Link to="#" className="nav-link">Contacto</Link>
-//                       </li>
-//                       {store.user ? (
-//                         <>
-//                           <li className="nav-item d-flex justify-content-end">
-//                               <Link to="/" className="nav-link text-danger" onClick={actions.logout}>Cerrar sesión</Link>
-//                           </li>
-//                           <li className="nav-item d-flex justify-content-end">
-//                               <Link to="/profile" className="nav-link text-warning">Mi perfil</Link>
-//                           </li> 
-//                         </> 
-//                       ) : (
-//                           <>
-//                               {location.pathname !== '/form-login' && (
-//                                   <li className="nav-item d-flex justify-content-end">
-//                                       <Link to="/form-login" className="nav-link text-white">Ingresar</Link>
-//                                   </li>
-//                               )}
-//                               {location.pathname !== '/form-signup' && (
-//                                   <li className="nav-item d-flex justify-content-end">
-//                                       <Link to="/form-signup" className="nav-link text-white">Registrarse</Link>
-//                                   </li>
-//                               )}
-//                           </>
-//                       )}
-//                   </ul>
-//               </div>
-//           </div>
-//       </nav>
-//   );
 };
 
 
