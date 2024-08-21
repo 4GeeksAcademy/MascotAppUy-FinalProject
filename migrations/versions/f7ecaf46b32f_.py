eb9c7d64f06762c7990564d95019904e756c789a
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-08-21 21:02:27 +0000

    d

--- migrations/versions/f7ecaf46b32f_.py
+++ migrations/versions/f7ecaf46b32f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0d297d764088
+Revision ID: f7ecaf46b32f
 Revises: 
-Create Date: 2024-08-21 17:19:26.624714
+Create Date: 2024-08-21 21:00:09.648395
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0d297d764088'
+revision = 'f7ecaf46b32f'
 down_revision = None
 branch_labels = None
 depends_on = None
