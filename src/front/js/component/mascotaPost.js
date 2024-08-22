151c9af309bf7fda7d0a5cc0cc7279a3d886deb1
Letimachado <leti.machado.gerez@gmail.com>
2024-08-22 15:14:03 +0000

    imágenes de mascotaPost y botón de contacto

--- src/front/js/component/mascotaPost.js
+++ src/front/js/component/mascotaPost.js
@@ -14,15 +14,15 @@ const MascotaPost = (props) => {
         <>
     {props.estado == "PERDIDO" ? (<h1 className="display-4 text-center" style={{marginTop: "20px"}}>Estoy perdido!</h1>) : 
     (<h1 className="display-4 text-center" style={{marginTop: "20px"}}>Estoy buscando a mi familia!</h1>)}
-    
-    <div className="d-flex py-5" style={{justifyContent: "center"}} id="perrito">
-        
-        <div>
-             <img src={props.imagen} className="img-fluid rounded" style={{ width: "100%", height: "100%", maxHeight: "350px", maxWidth:"350px" }} alt="Mascota" />
+    <div className="d-flex" style={{justifyContent: "center", alignItems: "center", height: "100vh"}}>
+    <div className="card mb-3" style={{maxWidth: "75%", width:"100%"}} id="perrito">
+        <div className="row g-0">
+        <div className="col-md-4">
+             <img src={props.imagen} className="imgFluid rounded" style={{ width: "100%", height: "100%", objectFit: "cover", maxHeight:"400px", maxWidth:"400px" }} alt="Mascota" />
         </div>
     
-        <div className="card" style={{width: '400px'}}>
-            <div className="card-body" style={{"display": "grid"}}>
+        <div className="col-md-8">
+            <div className="cardBody" style={{"display": "grid", padding: "20px"}}>
                 {props.estado == "PERDIDO" ? 
                 (<><p>Nombre: {props.nombre}</p>
                 <p>Especie: {props.especie}</p>
@@ -32,7 +32,7 @@ const MascotaPost = (props) => {
                 <p>Fecha de perdido: {props.fechaPerdido}</p>
                 <p>Descripción: {props.descripcion}</p>
                 <p>Se perdió en: {props.localidad}, {props.departamento}</p>
-                <button style={{"justifyContent": "center", border: "none", backgroundColor: "orange"}}>Contactar al dueño</button>
+                <button style={{ border: "none", backgroundColor: "orange", padding: "5px 10px", width: "40%", margin: "0 auto" }}>Contactar al dueño</button>
                 </>
                 ) : (<> <p>Título: {props.nombre}</p>
                     <p>Especie: {props.especie}</p>
@@ -40,13 +40,14 @@ const MascotaPost = (props) => {
                     <p>Fecha de registro: {props.fechaReg}</p>
                     <p>Descripción: {props.descripcion}</p>
                     <p>Fue encontrado en: {props.localidad}, {props.departamento}</p>
-                    <button style={{"justifyContent": "center", border: "none", backgroundColor: "orange"}}>Soy el dueño</button>
+                    <button style={{ border: "none", backgroundColor: "orange", padding: "5px 10px", width: "40%", margin: "0 auto" }}>Soy el dueño</button>
                     </>
                 )}
 
                 
-                
+              </div>  
              </div>
+        </div>
         </div>
     </div>
      
