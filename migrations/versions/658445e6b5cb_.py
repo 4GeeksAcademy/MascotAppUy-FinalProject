f30c6da19de47583923103351e9bc8bb5371eb3f
agusotoholt <168455366+agusotoholt@users.noreply.github.com>
2024-08-29 15:38:40 +0000

    varios

--- migrations/versions/658445e6b5cb_.py
+++ migrations/versions/658445e6b5cb_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 20d400ea63a2
+Revision ID: 658445e6b5cb
 Revises: 
-Create Date: 2024-08-29 12:25:57.755042
+Create Date: 2024-08-29 13:39:06.207980
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '20d400ea63a2'
+revision = '658445e6b5cb'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,7 @@
 //Si hiciste git pull o cambiaste de codespace, hay que cambiar el link y crear nuevas mascotas
 
 // const URL = process.env.BACKEND_URL
-const URL = "https://cautious-goggles-97x45pp9r473p6x-3001.app.github.dev"
+const URL = "https://silver-space-zebra-5gv659w4pq9gfppj6-3001.app.github.dev"
 
 
 const getState = ({ getStore, getActions, setStore }) => {
