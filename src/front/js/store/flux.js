e90c700076f2842bf8f1a858cc2713d6a79489d0
Trilce Godoy <112527761+tgodoy12@users.noreply.github.com>
2024-08-30 14:50:52 -0300

    Update flux.js

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,7 @@
 //Si hiciste git pull o cambiaste de codespace, hay que cambiar el link y crear nuevas mascotas
 
-// const URL = process.env.BACKEND_URL
-const URL = "https://stunning-trout-p46xprx5pprh647j-3001.app.github.dev"
+const URL = process.env.BACKEND_URL
+// const URL = "https://stunning-trout-p46xprx5pprh647j-3001.app.github.dev"
 
 
 const getState = ({ getStore, getActions, setStore }) => {
